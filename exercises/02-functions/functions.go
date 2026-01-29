518c8e4fa34a6a251c3d4afbd4420989dcf1b539
Gary Lai <garylai@amazon.com>
2026-01-29 11:39:14 -0800

    chore: finish exercise 2

--- exercises/02-functions/functions.go
+++ exercises/02-functions/functions.go
@@ -12,13 +12,15 @@ import "errors"
 // In JS: return { quotient, remainder } or return [quotient, remainder]
 func Divide(a, b int) (int, int) {
 	// TODO: return a/b and a%b
-	return 0, 0
+	return a / b, a % b
 }
 
 // 2. Named return values with naked return
 // Go lets you name return values and use "return" without arguments
 func DivideNamed(a, b int) (quotient, remainder int) {
 	// TODO: assign to quotient and remainder, then just "return"
+	quotient = a / b
+	remainder = a % b
 	return
 }
 
@@ -28,7 +30,10 @@ func DivideNamed(a, b int) (quotient, remainder int) {
 func SafeDivide(a, b int) (int, error) {
 	// TODO: if b is 0, return 0 and errors.New("cannot divide by zero")
 	// Otherwise return a/b and nil
-	return 0, nil
+	if b != 0 {
+		return a / b, nil
+	}
+	return 0, errors.New("cannot divide by zero")
 }
 
 // 4. Functions as values (first-class functions)
@@ -39,14 +44,26 @@ func GetOperation(op string) func(int, int) int {
 	// "subtract" -> returns a - b
 	// "multiply" -> returns a * b
 	// default -> returns function that returns 0
-	return nil
+	switch op {
+	case "add":
+		return func(a int, b int) int { return a + b }
+	case "subtract":
+		return func(a int, b int) int { return a - b }
+	case "multiply":
+		return func(a int, b int) int { return a * b }
+	}
+	return func(a int, b int) int { return 0 }
 }
 
 // 5. Variadic functions (like JS rest parameters)
 // In JS: function sum(...numbers) { return numbers.reduce((a,b) => a+b, 0) }
 func Sum(numbers ...int) int {
 	// TODO: sum all numbers using range
-	return 0
+	sum := 0
+	for _, v := range numbers {
+		sum += v
+	}
+	return sum
 }
 
 // 6. Closure - function that captures outer variable
@@ -54,14 +71,24 @@ func Sum(numbers ...int) int {
 func MakeCounter() func() int {
 	// TODO: return a function that increments and returns a counter
 	// Each call should return 1, 2, 3, ...
-	return nil
+	n := 0
+	return func() int { n++; return n }
 }
 
 // 7. Higher-order function - takes a function as parameter
 // In JS: array.map(fn)
 func MapInts(numbers []int, fn func(int) int) []int {
 	// TODO: apply fn to each number and return new slice
-	return nil
+	// res := []int{}
+	// for _, v := range numbers {
+	// 	res = append(res, fn(v))
+	// }
+	// return res
+	res := make([]int, len(numbers)) // Pre-allocate exact size
+	for i, v := range numbers {
+		res[i] = fn(v) // Direct assignment, no append
+	}
+	return res
 }
 
 // Keep import used
